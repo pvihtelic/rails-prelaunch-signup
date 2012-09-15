d48ec17cbf3c5c9253994616e4ae1d60fd708910
pvihtelic <patrick.vihtelic@gmail.com>
2012-09-14 20:50:16 -0700

    implement 'Request Invitation' feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,7 @@ RailsPrelaunchSignup::Application.routes.draw do
   end
   devise_scope :user do
     root :to => "devise/registrations#new"
-    match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
   end
-  devise_for :users, :controllers => { :registrations => "registrations", :confirmations => "confirmations" }
-  match 'users/bulk_invite/:quantity' => 'users#bulk_invite', :via => :get, :as => :bulk_invite
-  resources :users, :only => [:show, :index] do
-    get 'invite', :on => :member
-  end
+  devise_for :users, :controllers => { :registrations => "registrations" }
+  resources :users, :only => [:show, :index]
 end
