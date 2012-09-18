cb5fef89ca338caefb996ad61b001c71f91b9d1f
pvihtelic <patrick.vihtelic@gmail.com>
2012-09-18 15:17:37 -0700

    implement 'View Progress' feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ RailsPrelaunchSignup::Application.routes.draw do
   end
   devise_scope :user do
     root :to => "devise/registrations#new"
+    match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
   end
-  devise_for :users
-  resources :users, :only => [:show, :index]
+  devise_for :users, :controllers => { :registrations => "registrations" }
+  match 'users/bulk_invite/:quantity' => 'users#bulk_invite', :via => :get, :as => :bulk_invite
+  resources :users, :only => [:show, :index] do
+    get 'invite', :on => :member
+  end
 end
--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -10,18 +10,10 @@ When /^I visit the users page$/ do
   visit users_path
 end
 
-When /^I click a link "([^"]*)"$/ do |arg1|
-  click_on (arg1)
-end
-
 Then /^I should see a list of users$/ do
   page.should have_content @user[:email]
 end
 
 Then /^I should see an access denied message$/ do
   page.should have_content "Not authorized as an administrator"
-end
-
-Then /^show me the page$/ do
-  save_and_open_page
 end
