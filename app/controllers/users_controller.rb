e95c8b62d00ca46adc8f008a42e4728056859faa
pvihtelic <patrick.vihtelic@gmail.com>
2012-09-18 16:07:43 -0700

    add chart to admin dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,22 +11,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
-  def invite
-    authorize! :invite, @user, :message => 'Not authorized as an administrator.'
-    @user = User.find(params[:id])
-    @user.send_confirmation_instructions
-    redirect_to :back, :notice => "Sent invitation to #{@user.email}."
-  end
-  
-  def bulk_invite
-    authorize! :bulk_invite, @user, :message => 'Not authorized as an administrator.'
-    users = User.where(:confirmation_token => nil).order(:created_at).limit(params[:quantity])
-    users.each do |user|
-      user.send_confirmation_instructions
-    end
-    redirect_to :back, :notice => "Sent invitation to #{users.count} users."
-  end
-  
   private
   
   def create_chart
@@ -35,7 +19,7 @@ class UsersController < ApplicationController
     data_table.new_column('date')
     data_table.new_column('number')
     users_by_day.each do |day|
-      data_table.add_row([ Date.parse(day[0]), day[1]])
+      data_table.add_row([ Date.parse(day[0].to_s), day[1]])
     end
     @chart = GoogleVisualr::Interactive::AnnotatedTimeLine.new(data_table)
   end
